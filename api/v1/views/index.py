e5835b9392cfc2d3bac6cd49373461f9eff4c866
Yidu-xSkript <yidu95@gmail.com>
2023-02-14 01:41:52 -0800

    modified git ignore

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,21 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the JSON status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Creates an endpoint that retrieves the # of each obj by type"""
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
