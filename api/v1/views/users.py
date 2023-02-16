967be65f7dde96a4421de98c6ef484bddc52d4fd
Yidu-xSkript <yidu95@gmail.com>
2023-02-15 23:26:54 -0800

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -26,8 +26,8 @@ def user_retrieval(user_id):
     """Retrieval of User objects with linked amenity ids"""
     user_dict = storage.all('User')
     user = user_dict.get('User' + "." + user_id)
-    if user is None:  # if user_id is not linked to any user obj
-        abort(404)  # then, raise 404 error
+    if user is None:
+        abort(404)
     else:
         return jsonify(user.to_dict()), 200
 
@@ -38,29 +38,29 @@ def delete_user(user_id):
     """Deletes a User object"""
     objects = storage.get('User', user_id)
     if objects is None:
-        abort(404)  # if the user_id is not linked to any User object
+        abort(404)
     else:
         storage.delete(objects)
         storage.save()
-    return jsonify({}), 200  # returns an empty dict with status code 200
+    return jsonify({}), 200
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """Create a User, from data provided by the request"""
-    body = request.get_json()  # transfrom the HTTP body request to dict
-    if not body:  # if HTTP body req is  not a valid JSON
-        abort(400, {"Not a JSON"})  # raise 400 err with the message Not a JSON
-    if 'email' not in body:  # if the dict doesn't contain the key email
-        abort(400, {"Missing email"})  # raise err and message
-    if 'password' not in body:  # if the dict doesn't contain the key passwd
-        abort(400, {"Missing password"})  # raise err and message
+    body = request.get_json()
+    if not body:
+        abort(400, {"Not a JSON"})
+    if 'email' not in body:
+        abort(400, {"Missing email"})
+    if 'password' not in body:
+        abort(400, {"Missing password"})
     objects = User()
     for key, value in body.items():
         setattr(objects, key, value)
     storage.new(objects)
     storage.save()
-    return jsonify(objects.to_dict()), 201  # returns new User
+    return jsonify(objects.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'],
@@ -71,11 +71,11 @@ def update_user(user_id):
     if not body:
         abort(400, {"Not a JSON"})
     objects = storage.get('User', user_id)
-    if objects is None:  # if user_id is not linked to any User object
+    if objects is None:
         abort(404)
-    ignore_keys = ['id', 'email', 'created_at', 'updated_at']  # ignore keys
-    for key, value in body.items():  # update User obj with key-val pairs
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in body.items():
         if key not in ignore_keys:
             setattr(objects, key, value)
     storage.save()
-    return jsonify(objects.to_dict()), 200  # return User obj
+    return jsonify(objects.to_dict()), 200
