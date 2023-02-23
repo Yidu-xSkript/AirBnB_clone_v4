3db269b63cbc0be7a472b56e4573f7ed771bbbec
Yidu-xSkript <yidu95@gmail.com>
2023-02-23 04:31:07 -0800

    v4 update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,18 +2,29 @@
 """A script that starts a Flask web application"""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
 from api.v1.views import app_views
 import os
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask import Blueprint
+from flasgger import Swagger
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+swagger = Swagger(app)
 
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+# app_views BluePrint defined in api.v1.views
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def app_teardown(self):
@@ -29,8 +40,54 @@ def page_not_found(Error):
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+        Global Route to handle All Error Status Codes
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST') or '0.0.0.0',
-            port=os.getenv('HBNB_API_PORT') or '5000',
-            threaded=True)
+    """"""
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
+    app.run(host=host, port=port)
